8646ab54de01b05679d81eb8e4ad56ba9c4d6f9f
Samuel Chibueze <lightworthng@gmail.com>
2020-04-14 11:20:37 +0100

    api-user-auth:: done and added gravatar

---
+++ middlewares/checkAuth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+const checkAuth = (req, res, next) => {
+ 
+    // Get token from header
+   const token = req.headers['x-auth-token'];
+ 
+   if (!token) {
+     return res.status(401).json({
+       status: false,
+       error: ['Auth Failed:: No token in header']
+     });
+   }
+
+  //  There is token, verify
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        status: false,
+        error: ['Auth Failed:: Invalid token']
+      });
+    }
+
+    // token was valid
+    req.currentUserId = decoded.id;
+    next();
+  });
+ 
+}
+
+module.exports = checkAuth;
