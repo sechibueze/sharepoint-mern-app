9b878ee3f79d30550ffe50d1022beb89e7012bb9
Samuel Chibueze <lightworthng@gmail.com>
2020-04-28 16:29:58 +0100

    api and client to heroku

--- server.js
+++ server.js
@@ -19,5 +19,13 @@ app.use('/api/posts', postsRoute);
 app.use('/', (req, res) => {
   return res.status(200).json({ message: 'Welcome to Educatus MERN app'});
 });
+if (process.env.NODE_ENV === 'production') {
 
+    // set static folder 
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+
+}
 app.listen(port, () => console.log(`Educatus::server running on ${port}`));
